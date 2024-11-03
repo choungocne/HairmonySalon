433036aa2dc3bacd0d6e9a58e71f9bccd380aaba
Chau Ngoc <chaungoc7909@gmail.com>
2024-11-04 00:00:22 +0700

    commit

--- HairmonySalon.Services/obj/Debug/net8.0/Harmony.Services.AssemblyInfo.cs
+++ HairmonySalon.Services/obj/Debug/net8.0/Harmony.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Harmony.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31981df8a34da21f9b5349f907331aace756cd5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7898a3f2b163917959fbdb4f1374a981ec5ef986")]
 [assembly: System.Reflection.AssemblyProductAttribute("Harmony.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Harmony.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
