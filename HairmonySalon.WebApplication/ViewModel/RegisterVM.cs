a559c71f4048873c94a5c1db46c578d04616632e
Chau Ngoc <chaungoc7909@gmail.com>
2024-11-04 19:15:57 +0700

    them

---
+++ HairmonySalon.WebApplication/ViewModel/RegisterVM.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HairHarmonySalon.ViewModel
+{
+	public class RegisterVM
+	{
+		[Required(ErrorMessage = "Username cannot be blank.")]
+		public string Username { get; set; }
+		[Required(ErrorMessage = "Password cannot be blank.")]
+	public string Password { get; set; }
+		[Required(ErrorMessage = "Confirm Password cannot be blank.")]
+		[Compare("Password", ErrorMessage = "Password and Confirm Password do not match.")]
+
+	public string ConfirmPassword { get; set; }
+	[Required(ErrorMessage = "Email cannot be blank.")]
+	[EmailAddress(ErrorMessage = "Invalid Email.")]
+	
+	public string Email { get; set; }
+	 public string Mobile { get; set; }
+	 public DateTime? DateOfBirth { get; set; }	
+	
+	public string Address { get; set; }
+
+}
+}
